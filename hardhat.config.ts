9a90c187355317031b934fe41cad07ee95d6757a
Ashish bhatt <117436056+Ashish0197@users.noreply.github.com>
2024-05-11 15:22:31 +0530

    hardhat config done

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,25 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import fs from "fs";
+
+const privateKey = fs.readFileSync(".secret").toString().trim();
+const url = fs.readFileSync(".alchemyUrl").toString().trim();
+const etherscan = fs.readFileSync(".etherscan").toString().trim();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    sepolia: {
+      url,
+      accounts: [`${privateKey}`],
+    },
+  },
+  etherscan: {
+    apiKey: etherscan,
+  },
+  sourcify: {
+    enabled: true,
+  },
 };
 
 export default config;
